a208385140a08266475699ec876d4aeaa420d037
Matheus Silva <msilvaqs@outlook.com>
2024-10-16 11:51:01 -0400

    Adding signin signup with google and github

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -89,31 +89,20 @@ const LoginModal = () => {
         outline
         label="Continue with Google"
         icon={FcGoogle}
-        onClick={() => {}}
+        onClick={() => signIn("google")}
       />
       <Button
         outline
         label="Continue with Github"
         icon={AiFillGithub}
-        onClick={() => {}}
+        onClick={() => signIn("github")}
       />
-      <div
-        className="
-                text-neutral-500 
-                text-center 
-                mt-4 
-                font-light
-                "
-      >
+      <div className="text-neutral-500 text-center mt-4 font-light">
         <p>
           Already have an account?
           <span
             onClick={onToggle}
-            className="
-                        text-neutral-800
-                        cursor-pointer 
-                        hover:underline
-                        "
+            className="text-neutral-800 cursor-pointer hover:underline"
           >
             {" "}
             Log in
--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -15,6 +15,7 @@ import Modal from "./Modal";
 import Input from "../inputs/Input";
 import Heading from "../Heading";
 import Button from "../Button";
+import { signIn } from "next-auth/react";
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal();
@@ -94,31 +95,20 @@ const RegisterModal = () => {
         outline
         label="Continue with Google"
         icon={FcGoogle}
-        onClick={() => {}}
+        onClick={() => signIn("google")}
       />
       <Button
         outline
         label="Continue with Github"
         icon={AiFillGithub}
-        onClick={() => {}}
+        onClick={() => signIn("github")}
       />
-      <div
-        className="
-                text-neutral-500 
-                text-center 
-                mt-4 
-                font-light
-                "
-      >
+      <div className="text-neutral-500 text-center mt-4 font-light">
         <p>
           Already have an account?
           <span
             onClick={onToggle}
-            className="
-                        text-neutral-800
-                        cursor-pointer 
-                        hover:underline
-                        "
+            className="text-neutral-800 cursor-pointer hover:underline"
           >
             {" "}
             Log in
