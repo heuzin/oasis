394d8388994e50936fa7bffdc6bfb827904f4ed9
Matheus Silva <msilvaqs@outlook.com>
2024-10-17 13:05:32 -0400

    Favorites page

---
+++ app/actions/getFavoriteListings.ts
@@ -0,0 +1,24 @@
+import { db } from "../libs/db";
+import { getCurrentUser } from "./getCurrentUser";
+
+export const getFavoriteListings = async () => {
+  try {
+    const currentUser = await getCurrentUser();
+
+    if (!currentUser) {
+      return [];
+    }
+
+    const favorites = await db.listing.findMany({
+      where: {
+        id: {
+          in: [...(currentUser.favoriteIds || [])],
+        },
+      },
+    });
+
+    return favorites;
+  } catch (error: any) {
+    throw new Error(error);
+  }
+};
---
+++ app/favorites/page.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import ClientOnly from "../components/ClientOnly";
+import EmptyState from "../components/EmptyState";
+import FavoritesClient from "./FavoritesClient";
+
+import { getFavoriteListings } from "../actions/getFavoriteListings";
+import { getCurrentUser } from "../actions/getCurrentUser";
+
+const FavoritesPage = async () => {
+  const listings = await getFavoriteListings();
+  const currentUser = await getCurrentUser();
+
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState
+          title="No favorites found"
+          subtitle="Looks like you have no favorite listings"
+        />
+      </ClientOnly>
+    );
+  }
+
+  return (
+    <ClientOnly>
+      <FavoritesClient listings={listings} currentUser={currentUser} />
+    </ClientOnly>
+  );
+};
+
+export default FavoritesPage;
