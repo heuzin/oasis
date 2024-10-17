48d2c3270dc01ca51b27a0a5089ace54086e746d
Matheus Silva <msilvaqs@outlook.com>
2024-10-17 09:17:29 -0400

    Listing by id page

---
+++ app/actions/getListingById.ts
@@ -0,0 +1,28 @@
+import { db } from "../libs/db";
+
+interface IParams {
+  listingId?: string;
+}
+
+export const getListingById = async (params: IParams) => {
+  try {
+    const { listingId } = params;
+
+    const listing = await db.listing.findUnique({
+      where: {
+        id: listingId,
+      },
+      include: {
+        user: true,
+      },
+    });
+
+    if (!listing) {
+      return null;
+    }
+
+    return listing;
+  } catch (error: any) {
+    throw new Error(error);
+  }
+};
