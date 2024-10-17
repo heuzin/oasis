82034a99d79c430fbd2f84a20435d2559d32e70e
Matheus Silva <msilvaqs@outlook.com>
2024-10-17 12:55:44 -0400

    Creating reservation screen

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -66,7 +66,10 @@ const UserMenu: FC<UserMenuProps> = ({ currentUser }) => {
                   label="My trips"
                 />
                 <MenuItem onClick={() => {}} label="My favorites" />
-                <MenuItem onClick={() => {}} label="My reservations" />
+                <MenuItem
+                  onClick={() => router.push("/reservations")}
+                  label="My reservations"
+                />
                 <MenuItem onClick={() => {}} label="My properties" />
                 <MenuItem onClick={rentModal.onOpen} label="Oasis my home" />
                 <hr />
