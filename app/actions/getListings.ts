dd449f9ea80f9713aa8aee4e497cdc559e344302
Matheus Silva <msilvaqs@outlook.com>
2024-10-16 22:39:38 -0400

    Displaying listings on home page

---
+++ app/actions/getListings.ts
@@ -0,0 +1,15 @@
+import { db } from "../libs/db";
+
+export const getListings = async () => {
+  try {
+    const listings = await db.listing.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    return listings;
+  } catch (error: any) {
+    throw new Error(error);
+  }
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,38 @@
-export default function Home() {
-  return <>Home</>;
+import { Listing } from "@prisma/client";
+
+import Container from "./components/Container";
+import ClientOnly from "./components/ClientOnly";
+import EmptyState from "./components/EmptyState";
+import ListingCard from "./components/listings/ListingCard";
+
+import { getListings } from "./actions/getListings";
+import { getCurrentUser } from "./actions/getCurrentUser";
+
+export default async function Home() {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
+
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    );
+  }
+
+  return (
+    <ClientOnly>
+      <Container>
+        <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+          {listings.map((listing: Listing) => (
+            <ListingCard
+              key={listing.id}
+              currentUser={currentUser}
+              data={listing}
+            />
+          ))}
+        </div>
+      </Container>
+    </ClientOnly>
+  );
 }
